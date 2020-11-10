473ebefad9c30689c4aedf59fdd3d85bdf53dd12
beatriz85 <beatriz85>
2020-11-10 11:33:54 +0100

    Creado ejercicio

---
+++ angular8/src/app/Components/lista-correo/lista-correo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-correo',
+  templateUrl: './lista-correo.component.html',
+  styleUrls: ['./lista-correo.component.scss']
+})
+export class ListaCorreoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- angular8/src/app/app.module.ts
+++ angular8/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './Components/correo/correo.component';
+import { ListaCorreosComponent } from './Components/lista-correos/lista-correos.component';
+import { ListaCorreoComponent } from './lista-correo/lista-correo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CorreoComponent
+    CorreoComponent,
+    ListaCorreosComponent,
+    ListaCorreoComponent
   ],
   imports: [
     BrowserModule,
